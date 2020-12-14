77dde05770c804512f955df0f677ba77708319cc
H09713M <46681374+H09713M@users.noreply.github.com>
2020-12-14 09:09:05 -0700

    Add files via upload

--- Lab 2 Solution.sql
+++ Lab 2 Solution.sql
@@ -1,40 +1,69 @@
--- 0. Drop table INSTRUCTOR in case it already exists
-drop table INSTRUCTOR
-;
---1. Create table INSTRUCTOR
-CREATE TABLE INSTRUCTOR
-  (ins_id INTEGER PRIMARY KEY NOT NULL, 
-   lastname VARCHAR(15) NOT NULL, 
-   firstname VARCHAR(15) NOT NULL, 
-   city VARCHAR(15), 
-   country CHAR(2)
-  )
-;
---2A. Insert single row for Rav Ahuja
-INSERT INTO INSTRUCTOR
-  (ins_id, lastname, firstname, city, country)
-  VALUES 
-  (1, 'Ahuja', 'Rav', 'Toronto', 'CA')
-;
---2B. Insert the two rows for Raul and Hima
-INSERT INTO INSTRUCTOR
-  VALUES
-  (2, 'Chong', 'Raul', 'Toronto', 'CA'),
-  (3, 'Vasudevan', 'Hima', 'Chicago', 'US')
-;
---3. Select all rows in the table
-SELECT * FROM INSTRUCTOR
-;
---3b. Select firstname, lastname and country where city is Toronto
-SELECT firstname, lastname, country from INSTRUCTOR where city='Toronto'
-;
---4. Change the city for Rav to Markham
-UPDATE INSTRUCTOR SET city='Markham' where ins_id=1
-;
---5. Delete the row for Raul Chong
-DELETE FROM INSTRUCTOR where ins_id=2
-;
---5b. Retrieve all rows from the table
-SELECT * FROM INSTRUCTOR 
-;
+-- Task 0: Drop the table INSTRUCTOR from the database in case it already exists, 
+-- so that we start from a clean state.
+DROP TABLE 
+  INSTRUCTOR;
 
+-- Task 1: Create the INSTRUCTOR table as defined above. Have the ins_id be the 
+-- primary key, and ensure the lastname and firstname are not null.
+CREATE TABLE 
+  INSTRUCTOR
+  (
+  ins_id INTEGER PRIMARY KEY NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
+  firstname VARCHAR(50) NOT NULL,
+  city VARCHAR(50),
+  country CHAR(2)
+  );
+
+-- Task 2A: Insert one row into the INSTRUCTOR table for the the 
+-- instructor Rav Ahuja.  
+INSERT INTO 
+  INSTRUCTOR
+VALUES 
+  (1, 'Ahuja', 'Rav', 'Toronto', 'CA' );
+
+-- Task 2B: Insert two rows at once in the INSTRUCTOR table for
+-- instructors Raul Chong and Hima Vasudevan.
+INSERT INTO 
+  INSTRUCTOR
+VALUES 
+  (2, 'Chong', 'Raul', 'Toronto', 'CA'),
+  (3, 'Vasudevan', 'Hima', 'Chicago', 'US');
+
+-- Task 3: Select all rows from the INSTRUCTOR table.
+SELECT 
+    *
+FROM 
+  INSTRUCTOR;
+
+-- Task 3B: Select the firstname, lastname and country 
+-- where the city is Toronto
+SELECT 
+  firstname,
+  lastname,
+  country
+FROM 
+  INSTRUCTOR 
+WHERE 
+  city = 'Toronto';
+
+-- Task 4: Update the row for Rav Ahuja and change his 
+-- city to Markham.
+UPDATE 
+  INSTRUCTOR
+SET 
+  city = 'Markham'
+WHERE 
+  ins_id = 1;
+
+-- Task 5: Delete the row for Raul Chong from the table.
+DELETE FROM 
+  INSTRUCTOR
+WHERE 
+  ins_id = 2;
+
+-- Task 5B: Retrieve all rows in the INSTRUCTOR table
+SELECT 
+  *
+FROM 
+  INSTRUCTOR;
